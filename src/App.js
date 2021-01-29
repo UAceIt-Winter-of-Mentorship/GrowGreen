bccc34d711cf77c71e11355bd2ca0d156b2a3713
SAKSHI <sakshijangra111000@gmail.com>
2021-01-30 00:47:50 +0530

    Added react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+//  The filename should be the same as the React component's, and should be written in PascalCase.
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is the Home Page.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
